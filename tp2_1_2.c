1d12aee0273e154c0709e5734ce45568857216e4
Mauricio <maurijo.suarez@gmail.com>
2023-03-23 19:23:38 -0300

    Comentarios

--- tp2_1_2.c
+++ tp2_1_2.c
@@ -1,4 +1,6 @@
 // codigo a completar
+//correccion para usar punteros
+//elimine la rama Opcion_2 dado q al cambia entre ramas se eliminaban los archivos de mi carpeta, por lo que cree opcion2
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
