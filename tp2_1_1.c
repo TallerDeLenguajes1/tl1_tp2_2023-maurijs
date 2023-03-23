9ac842a1e9b67615e4f24c2b297e5458926ead88
Mauricio <maurijo.suarez@gmail.com>
2023-03-23 17:42:56 -0300

    Correcion tp2_1_1.c

--- tp2_1_1.c
+++ tp2_1_1.c
@@ -1,12 +1,16 @@
 // codigo a completar
 #include <stdio.h>
+#include <stdlib.h>
+
 #define N 20
+
 int main() {
 int i;
 double vt[N];
+
 for(i = 0; i<N ; i++)
 {
 vt[i]=1+rand()%100;
-printf("%.2f", vt[i]);
+printf("%f", vt[i]);
 }
 }
