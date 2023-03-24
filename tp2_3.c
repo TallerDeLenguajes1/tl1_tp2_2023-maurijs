5f71296e6702cb6aa8f7ddc104618fcff58b414a
Mauricio <maurijo.suarez@gmail.com>
2023-03-24 00:00:47 -0300

    Resolucion ejercicio tp2_3

--- tp2_3.c
+++ tp2_3.c
@@ -1,22 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define N 5
-#define M 7
+#define N 4
+#define M 6
 
-int main() {
-int i,j;
-int mt[N][M];
-int *punt = mt;
-srand(time(NULL));
-
-for(i = 0;i<N; i++)
+int main()
 {
-    for(j = 0;j<M; j++)
+    int i, j;
+    int mt[N][M];
+    int *punt;
+    punt = &mt[0][0];
+    srand(time(NULL));
+
+    for (i = 0; i < N ; i++)
     {
-    *punt[i][j]=(int)(1+rand()%100);
-    printf("%f",punt[i][j]);
+        for (int j = 0; j < M; j++)
+        {
+            *punt = 1 + rand() % 100;
+            printf("   mt[%d][%d]= %d   ",i,j,*punt);
+            punt++;
+        }
+        punt++;
+        printf("\n-----------------------------\n");
     }
-printf(“\n”);
 }
-}
+// si por ejemplo tengo una matriz m[F][C]
+// int*p = &mt[0][0];
+// para obtener el elemento mt[n][m] con el puntero aplico la formula
+// mt[n][m] == *(p+col*n+m) con esta formula llegamos a la ubicacion del elemento mt[n][m]
+//
